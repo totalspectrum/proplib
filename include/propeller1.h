844741fe0ceb140ab2fdf9d0667f68c1c39c31da
David Betz <dbetz@xlisper.com>
2016-08-05 10:06:49 -0400

    Add constants needed by clkset().

--- include/propeller1.h
+++ include/propeller1.h
@@ -178,6 +178,19 @@ static __inline__ void togglepin(int pin)
 /// Video Scale register for setting pixel and frame clocks.
 #define VSCL    _VSCL
 
+/// Constants uses with clkset()
+#define RCFAST	0x00
+#define RCSLOW  0x01
+#define XINPUT  0x22
+#define XTAL1   0x2a
+#define XTAL2   0x32
+#define XTAL3   0x3a
+#define PLL1X   0x41
+#define PLL2X   0x42
+#define PLL4X   0x43
+#define PLL8X   0x44
+#define PLL16X  0x45
+
 #ifdef __cplusplus
 }
 #endif
